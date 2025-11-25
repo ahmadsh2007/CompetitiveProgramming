d8cbdcf088c792dfd418e0e018da8683c8c44bf8
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-11-25 23:27:51 +0300

    Day 005 Work Day 004 Commit 06

---
+++ codeforces/JUST ACM Training 2024 - Beginner level - Group 4/H.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+
+    map<int, int> a;
+    int mx = 0;
+    for (int i = 0; i < n; i++){
+        int x; cin >> x;
+        a[x]++;
+    }
+
+    for (auto x : a) mx = max(mx, x.second);
+
+    cout << mx << ' ' << a.size();
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+    #endif
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
