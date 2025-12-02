c8d8bb5019114ee97924729a0772df141fafdbaa
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-12-02 19:15:12 +0300

    Day 012 Work Day 010 Commit 07

---
+++ codeforces/A/1881.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long; 
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+bool check(string s, string x){
+    if (x.size() < s.size()) return false;
+    for(int i = 0; i < x.size() - s.size() + 1; i++)
+        if (x.substr(i, s.size()) == s) return true;
+    return false;
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    str x, s; cin >> x >> s;
+
+    int ans = -1;
+    str xx = x + x;
+    str xxx = xx + xx;
+    str xxxx = xxx + xxx;
+    str xxxxx = xxxx + xxxx;
+    str xxxxxx = xxxxx + xxxxx;
+
+    if (check(s, x)) ans = 0;
+    else if (check(s, xx)) ans = 1;
+    else if (check(s, xxx)) ans = 2;
+    else if (check(s, xxxx)) ans = 3;
+    else if (check(s, xxxxx)) ans = 4;
+    else if (check(s, xxxxxx)) ans = 5;
+
+    cout << ans << '\n';
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
