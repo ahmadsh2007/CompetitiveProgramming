53a76586d75e4e5307d4b21d25a13972d971ea0f
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-12-03 16:56:59 +0300

    Day 013 Work Day 011 Commit 01

---
+++ codeforces/B/1862.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+
+    vector<int> ans;
+    ans.push_back(a[0]);
+    for (int i = 1; i < n; i++) {
+        if (a[i] >= a[i - 1]) ans.push_back(a[i]);
+        else {
+            ans.push_back(a[i]);
+            ans.push_back(a[i]);
+            
+        }
+    }
+
+    cout << ans.size() << endl;
+    for (auto x : ans) cout << x << ' ';
+    cout << '\n';
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
