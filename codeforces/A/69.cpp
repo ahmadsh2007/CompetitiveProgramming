b054905f73669c923bf24c3a0d5a3a15c1bfb32c
Leeking <leeking@pc-795.home>
2025-12-10 10:19:54 +0300

    Day 020 Work Day 018 Commit 03

---
+++ codeforces/A/69.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    int vectors[n][3];
+    int x = 0, y = 0, z = 0;
+    for (int i = 0; i < n; i++){
+        cin >> vectors[i][0];
+        cin >> vectors[i][1];
+        cin >> vectors[i][2];
+        x += vectors[i][0];
+        y += vectors[i][1];
+        z += vectors[i][2];
+    }
+
+    if (!x && !y && !z) cout << "YES";
+    else cout << "NO";
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
