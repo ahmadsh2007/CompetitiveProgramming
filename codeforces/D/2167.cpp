be00be21e37285fc3fec4e959bac0a73872c4c04
Leeking <leeking@wifibridge-2.home>
2025-12-13 16:47:54 +0300

    Day 023 Work Day 021 Commit 02

---
+++ codeforces/D/2167.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53};
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+    for (int p : primes)
+        for (auto x : a)
+            if (x % p) {
+                cout << p << '\n';
+                return;
+            }
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
