6eab5b2cdcb8fab40b903a4e5c4da837271c803d
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-12-13 21:26:57 +0300

    Day 023 Work Day 021 Commit 05

---
+++ codeforces/A/1869.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+    if (n & 1) {
+		cout << "4" << '\n';
+		cout << "1 " << n - 1 << '\n';
+		cout << "1 " << n - 1 << '\n';
+		cout << n - 1 << ' ' << n << '\n';
+		cout << n - 1 << ' ' << n << '\n';
+	} else {
+		cout << "2" << '\n';
+		cout << "1 " << n << '\n';
+		cout << "1 " << n << '\n';
+	}
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
