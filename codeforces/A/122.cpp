07b8f23428f2aec6d91ecdf6e2f204667a2436af
Leeking <leeking@pc-229.home>
2025-12-17 23:49:22 +0300

    Day 027 Work Day 024 Commit 01

---
+++ codeforces/A/122.cpp
@@ -0,0 +1,63 @@
+// Always test it before submitting it dumb Ahmad
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+
+    int lucky[] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+    bool isLucky = 0;
+    
+    for (auto l : lucky)
+        if (!(n % l)) { cout << "YES"; return; }
+
+    cout << "NO";
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Ahmad"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
