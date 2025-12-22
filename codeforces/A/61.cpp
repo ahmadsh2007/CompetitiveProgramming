e997723b743408c1e74419353aa38a4c8dddb9d1
Leeking <leeking@pc-229.home>
2025-12-22 18:00:47 +0300

    Day 032 Work Day 028 Commit 04

---
+++ codeforces/A/61.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    str a, b; cin >> a >> b;
+    str ans;
+    for (int i = 0; i < a.size(); i++) ans += (a[i] == b[i] ? '0' : '1');
+
+    cout << ans;
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
