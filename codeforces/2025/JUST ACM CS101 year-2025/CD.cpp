398ed02abd67d72a3283cffd406b8093c3b30be2
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-12-26 14:35:07 +0300

    Day 036 Work Day 032 Commit 02

---
+++ codeforces/2025/JUST ACM CS101 year-2025/CD.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define _vi1(a) vector<int> a;
+#define _vi2(a,n) vector<int> a(n);
+#define GET_MACRO(_1,_2,NAME,...) NAME
+#define vi(...) GET_MACRO(__VA_ARGS__, _vi2, _vi1)(__VA_ARGS__)
+#define read(v) for (auto &x : v) cin >> x;
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vi(a, n) read(a)
+    vi(b, m) read(b)
+    sort(b.begin(), b.end());
+
+    int last = LLONG_MIN;
+    int ok = 1;
+    for (int i = 0; i < n; i++) {
+        int chosen = LLONG_MAX;
+        if (a[i] >= last) chosen = a[i];
+
+        int wanted = last + a[i];
+        auto it = lower_bound(all(b), wanted);
+        if (it != b.end()) {
+            int value = *it - a[i];
+            if (value >= last) chosen = min(chosen, value);
+        }
+
+        if (chosen == LLONG_MAX) { ok = 0; break; }
+        last = chosen;
+    }
+
+    if (ok) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
