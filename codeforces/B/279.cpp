02670ba134517b396bdc219106ceadee6f351b52
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-12-28 17:48:22 +0300

    Day 038 Work Day 034 Commit 07

---
+++ codeforces/B/279.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define _vi1(a) vector<int> a;
+#define _vi2(a,n) vector<int> a(n);
+#define GET_MACRO(_1,_2,NAME,...) NAME
+#define vi(...) GET_MACRO(__VA_ARGS__, _vi2, _vi1)(__VA_ARGS__)
+#define read(v) for (auto &x : v) cin >> x;
+
+using i64 = long long;
+using ll = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, t; cin >> n >> t;
+    vi(a, n) read(a)
+    vi(pre, n + 1)
+    for (int i = 0; i < n; i++) pre[i + 1] = pre[i] + a[i]; 
+
+    int ans = (a[0] > t ? 0 : 1);
+    int l = 1;
+    for(int r = 2; r <= n; r++) {
+        while (pre[r] - pre[l - 1] > t) l++;
+        ans = max(ans, r + 1 - l);
+    }
+
+    cout << ans;
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
