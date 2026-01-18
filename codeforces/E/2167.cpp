b092c20ec335e15cd1f60d949e266dfdf39d567a
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-01-18 13:51:57 +0300

    Day 059 Work Day 055 Commit 02

---
+++ codeforces/E/2167.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define _vi1(a) vector<int> a;
+#define _vi2(a,n) vector<int> a(n);
+#define GET_MACRO(_1,_2,NAME,...) NAME
+#define vi(...) GET_MACRO(__VA_ARGS__, _vi2, _vi1)(__VA_ARGS__)
+#define _vill1(a) vector<long long> a;
+#define _vill2(a,n) vector<long long> a(n);
+#define GET_MACRO(_1,_2,NAME,...) NAME
+#define vill(...) GET_MACRO(__VA_ARGS__, _vill2, _vill1)(__VA_ARGS__)
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using i64 = long long;
+using ll  = long long;
+using u64 = unsigned long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, k, x; cin >> n >> k >> x;
+    vi(a, n) read(a)
+    a.push_back(-1e9);
+    a.push_back(1e9);
+    sort(all(a)); n += 2;
+    int l = 0, r = x + 1, mid;
+    while(l + 1 < r) {
+        int mid = (l + r) >> 1, ans = 0;
+        a[0] = -mid, a[n - 1] = x + mid;
+        for (int i = 1; i < n; i++) ans += max(0LL, (a[i] - mid) - (a[i - 1] + mid) + 1);
+        if (ans >= k) l = mid;
+        else r = mid;
+    }
+
+    a[0] = -l, a[n - 1] = l + x;
+    int c = 0;
+    for (int i = 1; i < n; i++) {
+        for (c = max(c, a[i - 1] + l); c <= min(x, a[i] - l) && k; c++) cout << c << ' ', k--;
+    }
+    cout << endl;
+}
+
+#undef int
+int main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
