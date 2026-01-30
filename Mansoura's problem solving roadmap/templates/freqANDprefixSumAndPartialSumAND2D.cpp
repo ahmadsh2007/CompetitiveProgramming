7ce866db593e77fdfdf7ac1d69108204c4b61dc0
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-01-30 12:09:49 +0300

    Day 071 Work Day 066 Commit 01

--- Mansoura's problem solving roadmap/templates/freqANDprefixSumAndPartialSumAND2D.cpp
+++ Mansoura's problem solving roadmap/templates/freqANDprefixSumAndPartialSumAND2D.cpp
@@ -25,4 +25,55 @@ int main()
     ll array[n] = {};
     ll prefixMax[n] = {};
     for (int i = 0; i < n; i++) prefixMax[i] = min(prefixMax[i - 1], array[i]);
+
+
+    // ! 2D PrefixSum
+    int size = 1005;
+    int arr[size][size];
+    int n, m; cin >> n >> m;
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            arr[i][j] += arr[i][j - 1];
+        }
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            arr[j][i] += arr[j - 1][i];
+        }
+    }
+
+    int up, left, down, right; cin >> up >> left >> down >> right;
+    cout << arr[down][right] - arr[up - 1][right] - arr[down][left - 1] + arr[up - 1][left - 1];
+
+    /*
+        Try input this, and the result must be 27:
+        4 4
+        6 7 8 9
+        5 4 2 3
+        4 7 8 3
+        5 3 3 3
+        2 2
+        3 4
+    */
+
+    // * Recap 2D PrefixSum steps
+    // Step 1: Make an 2D array for prefix
+    // Step 2: PrefixSum for each row
+    // Step 3: PrefixSum for each column
+    // Step 4: Calculate the Answer,
+    //         Answer = pre[Down][Right]   - pre[Down][Left - 1]
+    //                - pre[Up - 1][Right] + pre[Up - 1][Left - 1]
 }
