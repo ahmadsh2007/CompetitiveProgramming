2d2eb72ca172b0a1f02d4ccc28f088b1da998b77
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-04 12:47:52 +0300

    Day 076 Work Day 071 Commit 05

---
+++ codeforces/B/2158.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../Utils/debug.h"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n), b(n); cin(a) cin(b)
+
+    vi L(n), R(n);
+    for (int i = 0; i < n; i++)
+        L[i] = (i && L[i - 1] > 0 ? L[i - 1] : 0LL) + a[i];
+    for (int i = n - 1; i >= 0; i--)
+        R[i] = (i < n - 1 && R[i + 1] > 0 ? R[i + 1] : 0LL) + a[i];
+
+    int ans = LLONG_MIN;
+    if (k & 1)
+    {
+        for (int i = 0; i < n; i++)
+            ans = max(ans, L[i] + R[i] - a[i] + b[i]);
+    }
+    else
+    {
+        for (int i = 0; i < n; i++) ans = max(ans, L[i]);
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef ONLINE_JUDGE
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
