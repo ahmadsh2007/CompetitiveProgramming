c75a4d19f7cd0099d45d095d196ec79f527ce03c
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-05 14:10:13 +0300

    Day 077 Work Day 072 Commit 03

--- template/Sorting.cpp
+++ template/Sorting.cpp
@@ -115,3 +115,25 @@ int main() {
 
     return 0;
 }
+
+
+/*
+void bubbleSort(vector<int>& arr, int type) {
+    int n = arr.size();
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                ans.push_back({type, j + 1});
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+      
+        if (!swapped)
+            break;
+    }
+}
+*/
