6c67fe6b6f6a61bffe22d92634e35f97a356e5d8
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-06 23:08:55 +0300

    Day 078 Work Day 073 Commit 03

---
+++ template/lambda.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    // ! General Foramt:
+    // * [](int a) -> int { return a - 1; };
+    // [] is start of lambda
+    // (int a) is Parameter list
+    // -> int is return type
+    // { return a - 1; } is body
+
+    // [&]:     capture all external variables by reference.
+    // []:      capture all external variables by value.
+    // [a, &b]: capture 'a' by value and 'b' by reference.
+}
