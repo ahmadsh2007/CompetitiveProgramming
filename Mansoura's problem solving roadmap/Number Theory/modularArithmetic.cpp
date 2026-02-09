bd9c2997a74a9c39e672db9b0d6ad3d6bf7ffc58
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-09 23:14:36 +0300

    Day 081 Work Day 076 Commit 05

---
+++ Mansoura's problem solving roadmap/Number Theory/modularArithmetic.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+signed main(){
+    // If it is not guaranteed that the number will be positive
+    // Use: (a % n + n) % n
+
+    // If it is guaranteed, use:
+    // a % n
+
+    // This is how to calculate the Remainder
+    // Remainder = a - n * floor(a / n)
+
+    // If a % n == 0, then a divisible by n
+    // if a % n == b % n, then (a - b) % n = 0
+
+    // (a % n) % n = a % n
+    // (n ^ x) % n = 0 for any x >= 0
+
+    // (-a) % n != a % n
+    // (((-a) % n) + (a % n)) % n = 0
+
+    // (a + b) % n = (a % n + b % n) % n
+    // (a + b + c) % n?
+    // let's say that d = a + b, then
+    // (a + b + c) % n = (d + c) % n = (d % n + c % n) % n
+    // (a + b + c) % n = ((a % n + b % n) % n + c % n) % n
+
+    // Let's say we have `int ans = 0` and want to add x multiple times, and each time mod it with y to prevent overflow
+    // What we can do is
+    // ans = (ans + x) % y, but notice that ans + x may overflow, so
+    // we use this: ans = (ans % y + x % y) % y
+    // We can also do: ```
+    // ans += x % y
+    // ans %= y
+    // ```
+}
