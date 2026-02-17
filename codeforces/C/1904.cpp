3c70cd7632abe1ab3408474281d8a6317d39ad78
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-17 23:04:34 +0300

    Day 089 Work Day 084 Commit 06

---
+++ codeforces/C/1904.cpp
@@ -0,0 +1,105 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n); cin >> a;
+
+    if (k > 2) { cout << "0\n"; return; }
+
+    vi diff;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            diff.push_back(abs(a[i] - a[j]));
+
+    sort(all(diff));
+    sort(all(a));
+
+    int ans = LLONG_MAX;
+    for (int i = 0; i < n; i++) ans = min(ans, a[i]);
+
+    ans = min(ans, diff[0]);
+
+    if (k == 2) {
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                int val = a[j] - a[i];
+                int idx = lower_bound(all(a), val) - a.begin();
+                if (idx < n) ans = min(ans, a[idx] - val);
+                if (idx > 0) ans = min(ans, val - a[idx - 1]);
+            }
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef ONLINE_JUDGE
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
