578394e5aea4e11e06831648d773102c5c984f1b
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-18 23:45:55 +0300

    Day 090 Work Day 085 Commit 03

---
+++ codeforces/C/1793.cpp
@@ -0,0 +1,113 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    vi a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    int l = 1, r = n;
+    int mn =1, mx =n;
+    bool changed = 1;
+    bool side = 1;
+    while (changed) {
+        changed = 0;
+        for (int i = 0; i < 2; i++) {
+            side = i;
+            if (side) {
+                if (a[l] == mn) {
+                    mn++, l++;
+                    changed = 1;
+                }
+                else if (a[l] == mx) {
+                    mx--, l++;
+                    changed = 1;
+                }
+            }
+            else {
+                if (a[r] == mn) {
+                    mn++, r--;
+                    changed = 1;
+                }
+                else if (a[r] == mx) {
+                    mx--, r--;
+                    changed = 1;
+                }
+            }
+        }
+    }
+
+    if (l <= r) cout << l << ' ' << r << endl;
+    else cout << "-1\n";
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef ONLINE_JUDGE
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
