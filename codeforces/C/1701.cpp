26c8de58c39e180e7be8db40da4f05eeaf816a50
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-26 22:43:30 +0300

    Day 098 Work Day 093 Commit 02

---
+++ codeforces/C/1701.cpp
@@ -0,0 +1,103 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+bool check(int n, int m, int mid, vector<int> & freq) {
+    int possible = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (mid < freq[i]) possible += mid;
+        else possible += freq[i] + (mid - freq[i]) / 2;
+    }
+
+    return possible >= m;
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vi a(m); cin >> a;
+
+    vi freq(n);
+    for (int i = 0; i < m; i++) freq[a[i] - 1]++;
+
+    int l = 0, r = 2 * m, mid, ans = 0;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (check(n, m, mid, freq)) {
+            ans = mid;
+            r = mid - 1;
+        } else l = mid + 1;
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef ONLINE_JUDGE
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
