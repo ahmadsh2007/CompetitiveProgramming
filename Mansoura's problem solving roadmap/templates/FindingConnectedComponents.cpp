feab73f43f7803578e1a3f742e0d137d33298e50
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-02-27 14:38:54 +0300

    Day 099 Work Day 094 Commit 03

---
+++ Mansoura's problem solving roadmap/templates/FindingConnectedComponents.cpp
@@ -0,0 +1,106 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+vector<vector<int>> adj, comp;
+vector<bool> vis;
+vector<int> tmp;
+
+void DFS(int u) {
+    vis[u] = 1;
+    tmp.push_back(u);
+    for (auto v : adj[u]) {
+        if (!vis[v]) DFS(v);
+    }
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    adj.assign(n + 1, {});
+    vis.assign(n + 1, 0);
+
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            ans++;
+            DFS(i);
+            comp.push_back(tmp);
+            tmp.clear();
+        }
+    }
+
+    for (auto v : comp) {
+        for (auto u : v) {
+            cout << u << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 0;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef ONLINE_JUDGE
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
