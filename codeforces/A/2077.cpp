a7b117dd05b4dadd4d5153f8c5cb4d95f9a3cc69
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-03-14 23:41:15 +0300

    Day 114 Work Day 109 Commit 01

---
+++ codeforces/A/2077.cpp
@@ -0,0 +1,91 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    vi b(2 * n); cin >> b;
+    sort(all(b));
+
+    vi a(2 * n + 1);
+
+    for (int i = 0; i < n + 1; i++) a[2 * i] = b[n + i - 1];
+    for (int i = 0; i < n - 1; ++i) a[2 * i + 1] = b[i];
+
+    int sum = accumulate(b.begin() + n - 1, b.end(), 0LL);
+    sum -= accumulate(b.begin(), b.begin() + n - 1, 0LL);
+    
+    a[2 * n - 1] = sum;
+    cout << a << endl;
+}
+
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
