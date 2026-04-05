0d346ff055c6f801125e3069d8224e68507176cb
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-04-05 20:17:21 +0300

    Work Day 120 Commit 01

---
+++ codeforces/A/1404.cpp
@@ -0,0 +1,96 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n, k; cin >> n >> k;
+    str s; cin >> s;
+
+    bool ok = 1;
+    for (int i = k; i < n; ++i) {
+        if (s[i] == '1') {
+            if (s[i % k] == '0') ok = 0;
+            s[i % k] = '1';
+        }
+        if (s[i] == '0') {
+            if (s[i % k] == '1') ok = 0;
+            s[i % k] = '0';
+        }
+    }
+
+    int zeroCnt = 0, onesCnt = 0;
+    for (int i = 0; i < k; ++i) if (s[i] == '1') onesCnt++; else if (s[i] == '0') zeroCnt++;
+    if (zeroCnt > k / 2 || onesCnt > k / 2) ok = 0;
+
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
