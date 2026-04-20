66c9cd996c51eae8119d39444e7688428f5c6f0c
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-04-20 18:24:09 +0300

    Work Day 131 Commit 05

---
+++ codeforces/A/2.cpp
@@ -0,0 +1,93 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+
+    vector<pair<string, int>> rounds(n);
+    map<string, int> score;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> rounds[i].first >> rounds[i].second;
+        score[rounds[i].first] += rounds[i].second;
+    }
+
+    int mx = LLONG_MIN;
+    for (auto &[name, finalScore] : score) {
+        mx = max(mx, finalScore);
+    }
+
+    map<string, int> curScore;
+    for (int i = 0; i < n; ++i) {
+        string name = rounds[i].first;
+        int curPlayerScore = rounds[i].second;
+
+        curScore[name] += curPlayerScore;
+
+        if (score[name] == mx && curScore[name] >= mx) {
+            cout << name << endl;
+            return;
+        }
+    }
+}
+
+const int TESTCASES = 0;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
