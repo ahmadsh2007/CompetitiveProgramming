634151b6f4d5d8ba441bb0699d60f998afa580f1
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-04-20 17:36:41 +0300

    Work Day 131 Commit 03

---
+++ codeforces/B/96.cpp
@@ -0,0 +1,104 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int ret(string n, string s, int fourCnt, int sevenCnt, int len) {
+    if ((int) s.size() == len) {
+        if (fourCnt == sevenCnt)
+            if ((int) s.size() > (int) n.size() || stoll(s) >= stoll(n))
+                return stoll(s);
+        return LLONG_MAX;
+    }
+
+    int res = LLONG_MAX;
+    res = min(res, ret(n, s + "4", fourCnt + 1, sevenCnt, len));
+    res = min(res, ret(n, s + "7", fourCnt, sevenCnt + 1, len));
+
+    return res;
+}
+
+void solve() {
+    str n; cin >> n;
+
+    int len = (int) n.size();
+    if (len % 2) len++;
+
+    int ans = LLONG_MAX;
+    
+    while (ans == LLONG_MAX) {
+        ans = ret(n, "", 0, 0, len);
+        len += 2;
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 0;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
