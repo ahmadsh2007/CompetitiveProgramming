809e5f04200670102b1fd2afcf33151ac9f03e52
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-04-27 18:30:13 +0300

    Work Day 135 Commit 01

---
+++ codeforces/C/982.cpp
@@ -0,0 +1,99 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+vector<vector<int>> adj;
+int ans = 0;
+
+int dfs(int u, int p) {
+    int curSubTreeSize = 1;
+
+    for (auto &v : adj[u]) {
+        if (v != p) {
+            int size = dfs(v, u);
+
+            if (size % 2 == 0) ans++;
+            else curSubTreeSize += size;
+        }
+    }
+
+    return curSubTreeSize;
+}
+
+void solve() {
+    int n; cin >> n;
+    
+    adj.resize(n + 1, {});
+    for (int i = 1; i < n; ++i) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    if (n % 2) { cout << "-1\n"; return; }
+
+    ans = 0;
+    dfs(1, 0);
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 0;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
