7c4d21ec2270ca829dcdc3d626534b7056353ee8
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-05-05 14:44:34 +0300

    Work Day 141 Commit 02

---
+++ local contests/PSUT Marathon 2026/I.cpp
@@ -0,0 +1,107 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void buildPrefixAndSuffix(const int n, const vector<int> & a, vector<int> & prefix, vector<int> & suffix) {
+    prefix[0] = a[0];
+    for (int i = 1; i < n; ++i) prefix[i] = min(prefix[i - 1], a[i]);
+
+    suffix[n - 1] = a[n - 1];
+    for (int i = n - 2; i >= 0; --i) suffix[i] = max(suffix[i + 1], a[i]);
+
+    return;
+}
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); cin >> a;
+    vi prefix(n);
+    vi suffix(n);
+
+    buildPrefixAndSuffix(n, a, prefix, suffix);
+
+    int ans = 0;
+    while (true) {
+        bool allSame = 1;
+        for (int i = 1; i < n; ++i) if (a[i] != a[i - 1]) allSame = 0;
+
+        if (allSame) { cout << ans << endl; return; }
+
+        ans++;
+        for (int i = 0; i < n; ++i) a[i] = prefix[i] ^ suffix[i];
+        buildPrefixAndSuffix(n, a, prefix, suffix);
+    }
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
