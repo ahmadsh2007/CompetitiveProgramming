d8e9827b65eb9ca19ebfe602ebebc4eb4e473a25
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-05-11 10:42:11 +0300

    Work Day 145 Commit 03

---
+++ codeforces/F/1227.cpp
@@ -0,0 +1,102 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    vector<int> freq(n + 1);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+        freq[a[i]]++;
+    }
+
+    vector<int> suffix(n + 1);
+    suffix[n] = freq[n];
+    for(int i = n - 1; i >= 1; i--) suffix[i] = suffix[i + 1] + freq[i];
+
+    int beforeChange = 0, afterChange = 0;
+    for(int i = 1; i <= n; i++) {
+        beforeChange += i * a[i];
+        afterChange += suffix[i] * (2 * n - suffix[i] + 1) / 2;
+    }
+
+    int cur = afterChange - beforeChange, mx = 0;
+    for(int i = 1; i <= n; i++) mx = max(mx, i - n + suffix[a[i]] - 1);
+
+    cout << cur + mx << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
