cc46193ca474385fcbc4d1865f8e958164ec7ec6
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-05-18 23:04:31 +0300

    Work Day 151 Commit 01

---
+++ local contests/JUST Individuals Programming Contest 2026/H.cpp
@@ -0,0 +1,93 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+int getMEX(const vector<int>& a,int L,int R){int len=R-L+1;vector<char> s(len+2);for(int i=L;i<=R;i++)if(a[i]>=0&&a[i]<=len)s[a[i]]=1;int m=0;while(s[m])m++;return m;}
+int getMEX(const vector<int>& a){return getMEX(a,0,(int)a.size()-1);}
+
+void solve() {
+    int n, m, k; cin >> n >> m >> k;
+
+    vi a(n), b(m); cin >> a >> b;
+    vi c = {a[0], b[0]};
+    for (int i = 0; i < n; ++i) if (a[i] != k) c.push_back(a[i]); else break;
+    for (int i = 0; i < m; ++i) if (b[i] != k) c.push_back(b[i]); else break;
+
+    int ans = getMEX(c);
+    if (ans == k) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+const int TESTCASES = 1;
+signed main() {
+    #ifndef ONLINE_JUDGE
+    #if __has_include("../../Utils/debug.h")
+      freopen("../../Utils/input.txt", "r", stdin);
+      freopen("../../Utils/output.txt", "w", stdout);
+    #else
+      freopen("../../../Utils/input.txt", "r", stdin);
+      freopen("../../../Utils/output.txt", "w", stdout);
+    #endif
+    #endif
+
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
