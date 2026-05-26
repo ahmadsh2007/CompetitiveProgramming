26c93f32a97b3567aee6b44fb1300661090bc28e
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-05-26 16:16:49 +0300

    Work Day 159 Commit 06

---
+++ codeforces/E/1703.cpp
@@ -0,0 +1,121 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        // Generates a random seed once per program execution
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    // Safe O(1) Map
+    // std::unordered_map<long long, int, custom_hash> safe_map;
+    // Safe O(1) Set
+    // std::unordered_set<long long, custom_hash> safe_set;
+    // Fastest, safest map for CP
+    // gp_hash_table<long long, int, custom_hash> fast_safe_map;
+};
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+#define vi   vector<int>
+#define vill vector<ll>
+#define read(v) for (auto &x : v) cin >> x;
+#define cin(v)  for (auto &x : v) cin >> x;
+#define cout(v) for (auto &x : v) cout << x << ' ';
+
+using ll  = long long;
+using ull = unsigned long long;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+// Macro to generate a random integer between l and r (inclusive)
+// To use: `ll my_random_number = rnd(1, 100);` or `int random_index = rnd(0, my_vector.size() - 1);`
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+#define rnd(l, r) std::uniform_int_distribution<ll>(l, r)(rng)
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int n; cin >> n;
+    vector<vector<char>> a(n, vector<char>(n));
+    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> a[i][j];
+
+    int ans = 0;
+    char corners[4];
+    for (int i = 0; i < n / 2; ++i) {
+        for (int j = 0; j < (n + 1) / 2; ++j) {
+            corners[0] = a[i][j];
+            corners[1] = a[j][n - 1 - i];
+            corners[2] = a[n - 1 - j][i];
+            corners[3] = a[n - 1 - i][n - 1 - j];
+
+            int cnt[2] = {0, 0};
+            for (int k = 0; k < 4; ++k) cnt[corners[k] - '0']++;
+
+            cerr << cnt[0] << ' ' << cnt[1] << endl;
+            ans += min(cnt[0], cnt[1]);
+        }
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    ull TTT = 1;
+    if (TESTCASES == 1) cin >> TTT;
+    else if (TESTCASES == 2) {
+    #ifndef LOCAL
+      cin >> TTT;
+    #endif
+    }
+    while (TTT--) solve();
+    return 0;
+}
