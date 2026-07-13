fabfda8f8af121f327ca83f934afe630ae24c032
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-07-13 20:32:54 +0300

    Work Day 187 Commit 04

---
+++ codeforces/C/1398.cpp
@@ -0,0 +1,88 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        // Generates a random seed once per program execution
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    // Safe O(1) Map
+    // std::unordered_map<long long, int, custom_hash> safe_map;
+    // Safe O(1) Set
+    // std::unordered_set<long long, custom_hash> safe_set;
+    // Fastest, safest map for CP
+    // gp_hash_table<long long, int, custom_hash> fast_safe_map;
+};
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    str s; cin >> s;
+    vector<int> a(n);
+    unordered_map<int, int, custom_hash> prefix;
+    prefix[0]++;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += (s[i] - '0');
+        prefix[sum - (i + 1)]++;
+    }
+
+    int ans = 0;
+    for (auto &[key, cnt] : prefix) {
+        ans += cnt * (cnt - 1) / 2;
+    }
+
+    cout << ans << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
