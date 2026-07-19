c31b717dddc8a5452a5060d30ef77be562ab9c1f
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-07-19 21:57:04 +0300

    Work Day 193 Commit 03

---
+++ LeetCode/1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// ! Copy from here
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashTable;
+
+        for (int i = 0; i < (int) nums.size(); ++i) {
+            int rem = target - nums[i];
+            
+            if (hashTable.find(rem) != hashTable.end()) {
+                return {i, hashTable[rem]};
+            }
+
+            hashTable[nums[i]] = i;
+        }   
+        return {};
+    }
+};
+// ! Copy ends here
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        Solution solver;
+    
+        int n; cin >> n;
+        vector<int> num(n);
+        for (auto &x : num) cin >> x;
+        int target; cin >> target
+    
+        auto ans = solver.twoSum(num, target);
+        cout << "Result: [";
+        cout << ans[0] << ", " << ans[1];
+        cout << "]\n";
+    }
+
+    return 0;
+}
