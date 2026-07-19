25888af590357116de0887be1f554ad96e09b757
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-07-19 23:19:18 +0300

    Work Day 193 Commit 09

---
+++ LeetCode/169.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// ! Copy from here
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+};
+// ! Copy ends here
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        Solution solver;
+    
+        int n; cin >> n;
+        vector<int> nums(n);
+        for (auto &x : nums) cin >> x;
+    
+        auto ans = solver.majorityElement(nums);
+        // cout << "Result: [";
+        cout << ans << endl;
+        // cout << "]\n";
+    }
+
+    return 0;
+}
