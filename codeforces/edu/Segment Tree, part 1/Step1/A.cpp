24dd4bb7925c350ab3bd9ec02e3e1df069e8ae89
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-07-23 23:42:13 +0300

    Work Day 194 Commit 03

---
+++ codeforces/edu/Segment Tree, part 1/Step1/A.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SegTree {
+    int size;
+    vector<long long> sums;
+    void init(int n) {
+        size = 1;
+        while (size < n) size <<= 1;
+        sums.assign(2 * size, 0ll);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            sums[x] = v;
+            return;
+        }
+
+        int mid = lx + (rx - lx) / 2;
+        if (i < mid) {
+            set(i, v, 2 * x + 1, lx, mid);
+        }
+        else {
+            set(i, v, 2 * x + 2, mid, rx);
+        }
+        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+        return;
+    }
+    void set(int i, int v) {
+        set(i, v, 0, 0, size);
+    }
+
+    long long sum(int l, int r, int x, int lx, int rx) {
+        if (lx >= r or l >= rx) return 0ll;
+        if (lx >= l and rx <= r) return sums[x];
+
+        int mid = lx + (rx - lx) / 2;
+        long long s1 = sum(l, r, 2 * x + 1, lx, mid);
+        long long s2 = sum(l, r, 2 * x + 2, mid, rx);
+        return s1 + s2;
+    }
+    long long sum(int l, int r) {
+        return sum(l, r, 0, 0, size);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int n, m; cin >> n >> m;
+    SegTree st;
+    st.init(n);
+
+    for (int i = 0; i < n; ++i) {
+        int v; cin >> v;
+        st.set(i, v);
+    }
+    
+    while (m--) {
+        int op; cin >> op;
+        if (op == 1) {
+            int i, v; cin >> i >> v;
+            st.set(i, v);
+        }
+        else {
+            int l, r; cin >> l >> r;
+            cout << st.sum(l, r) << endl;
+        }
+    }
+}
---
+++ codeforces/edu/Segment Tree, part 1/Step1/C.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SegTree {
+    int size;
+    vector<long long> mins;
+    vector<long long> mCnt;
+    void init(int n) {
+        size = 1;
+        while (size < n) size <<= 1;
+        mins.assign(2 * size, 1e9 + 5);
+        mCnt.assign(2 * size, 0ll);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            mins[x] = v;
+            mCnt[x] = 1;
+            return;
+        }
+
+        int mid = lx + (rx - lx) / 2;
+        if (i < mid) {
+            set(i, v, 2 * x + 1, lx, mid);
+        }
+        else {
+            set(i, v, 2 * x + 2, mid, rx);
+        }
+        mins[x] = min(mins[2 * x + 1], mins[2 * x + 2]);
+        mCnt[x] = 0;
+        if (mins[x] == mins[2 * x + 1]) mCnt[x] += mCnt[2 * x + 1];
+        if (mins[x] == mins[2 * x + 2]) mCnt[x] += mCnt[2 * x + 2];
+        return;
+    }
+    void set(int i, int v) {
+        set(i, v, 0, 0, size);
+    }
+
+    long long getMin(int l, int r, int x, int lx, int rx) {
+        if (lx >= r or l >= rx) return 1e9 + 5;
+        if (lx >= l and rx <= r) return mins[x];
+
+        int mid = lx + (rx - lx) / 2;
+        long long m1 = getMin(l, r, 2 * x + 1, lx, mid);
+        long long m2 = getMin(l, r, 2 * x + 2, mid, rx);
+        return min(m1, m2);
+    }
+    long long getMin(int l, int r) {
+        return getMin(l, r, 0, 0, size);
+    }
+
+    long long countMin(int l, int r, int v, int x, int lx, int rx) {
+        if (lx >= r or l >= rx) return 0ll;
+        if (mins[x] > v) return 0ll; 
+        
+        if (lx >= l and rx <= r) {
+            return (mins[x] == v) ? mCnt[x] : 0ll;
+        }
+
+        int mid = lx + (rx - lx) / 2;
+        long long c1 = countMin(l, r, v, 2 * x + 1, lx, mid);
+        long long c2 = countMin(l, r, v, 2 * x + 2, mid, rx);
+        return c1 + c2;
+    }
+    long long countMin(int l, int r, int v) {
+        return countMin(l, r, v, 0, 0, size);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int n, m; cin >> n >> m;
+    SegTree st;
+    st.init(n);
+
+    for (int i = 0; i < n; ++i) {
+        int v; cin >> v;
+        st.set(i, v);
+    }
+    
+    while (m--) {
+        int op; cin >> op;
+        if (op == 1) {
+            int i, v; cin >> i >> v;
+            st.set(i, v);
+        }
+        else {
+            int l, r; cin >> l >> r;
+            int mn = st.getMin(l, r);
+            cout << mn << ' ';
+            cout << st.countMin(l, r, mn) << endl;
+        }
+    }
+}
