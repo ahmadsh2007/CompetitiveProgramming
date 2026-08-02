23124e1cc4fd9203237bc6a5c351dca61cb99d82
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-02 14:35:18 +0300

    Work Day 201 Commit 01

---
+++ codeforces/edu/Segment Tree, part 1/Step 4/A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SegTree {
+    int size;
+    vector<long long> sums;
+    void init(int n) {
+        size = 1;
+        while (size <= n) size <<= 1;
+        sums.assign(2 * size, 0ll);
+    }
+
+    void set(int i , int v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            sums[x] = v;
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        if (i < mid) {
+            set(i, v, 2 * x + 1, lx, mid);
+        }
+        else {
+            set(i, v, 2 * x + 2, mid, rx);
+        }
+
+        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+    }
+    void set(int i, int v) {
+        set(i, v, 0, 0, size);
+    }
+
+    long long getSum(int l, int r, int x, int lx, int rx) {
+        if (l >= rx or r <= lx) return 0ll;
+        if (lx >= l and rx <= r) return sums[x];
+
+        int mid = (lx + rx) / 2;
+        long long s1 = getSum(l, r, 2 * x + 1, lx, mid);
+        long long s2 = getSum(l, r, 2 * x + 2, mid, rx);
+        return s1 + s2;
+    }
+    long long getSum(int l, int r) {
+        if (l % 2) return -getSum(l, r, 0, 0, size);
+        return getSum(l, r, 0, 0, size);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n; cin >> n;
+
+    SegTree st;
+    st.init(n);
+
+    for (int i = 0; i < n; ++i) {
+        int v; cin >> v;
+        if (i % 2) v = -v;
+        st.set(i, v);
+    }
+
+    int q; cin >> q;
+    while (q--) {
+        int op; cin >> op;
+        if (op) {
+            int l, r; cin >> l >> r;
+            cout << st.getSum(l - 1, r) << endl;
+        }
+        else {
+            int i, v; cin >> i >> v;
+            if (i % 2 == 0) v = -v;
+            st.set(i - 1, v);
+        }
+    }
+}
