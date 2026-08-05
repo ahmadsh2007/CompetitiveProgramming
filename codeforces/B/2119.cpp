11056b24bc0b7c67f05d26b99ba57ec8a90ed451
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-05 23:17:00 +0300

    Work Day 204 Commit 02

---
+++ codeforces/B/2119.cpp
@@ -0,0 +1,67 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int n; cin >> n;
+    int px, py, qx, qy; cin >> px >> py >> qx >> qy;
+    vector<int> a(n); cin >> a;
+
+    double dis = (px - qx) * (px - qx) + (py - qy) * (py - qy);
+    dis = sqrt(dis);
+
+    int tot = accumulate(all(a), 0ll);
+    int mx = *max_element(all(a));
+
+    int mn = max(0ll, 2 * mx - tot);
+
+    if (dis >= mn and dis <= tot) return void(cout << "Yes\n");
+
+    cout << "No\n";
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
