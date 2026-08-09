d99999d6747517a9a3c740687cc8bf2651606012
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-09 13:13:27 +0300

    Work Day 207 Commit 02

---
+++ codeforces/F/2050Second.cpp
@@ -0,0 +1,131 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+template<typename T, typename Op = std::plus<T>>
+class SegTree {
+    /*
+    Recursive init
+    SegTree<long long> sum_tree(a, 0LL); // Defaults to std::plus
+    SegTree<long long, std::max<long long>> max_tree(a, -1e18);
+    SegTree<int, decltype([](const int & x, const int & y) -> int { return gcd(x, y); })> maximumModuloEquality(d, 0ll);
+    */
+private:
+    int size;
+    vector<T> tree;
+    T def;
+    Op op;
+
+    void build(int x, int lx, int rx, const vector<T>& a) {
+        if (rx - lx == 1) {
+            if (lx < (int) a.size()) tree[x] = a[lx];
+            return;
+        }
+        int mid = lx + (rx - lx) / 2;
+        build(2 * x + 1, lx, mid, a);
+        build(2 * x + 2, mid, rx, a);
+        tree[x] = op(tree[2 * x + 1], tree[2 * x + 2]);
+    }
+
+    void update(int i, T v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            tree[x] = v;
+            return;
+        }
+        int mid = lx + (rx - lx) / 2;
+        if (i < mid) update(i, v, 2 * x + 1, lx, mid);
+        else         update(i, v, 2 * x + 2, mid, rx);
+        tree[x] = op(tree[2 * x + 1], tree[2 * x + 2]);
+    }
+
+    T query(int l, int r, int x, int lx, int rx) {
+        if (lx >= r or  rx <= l) return def;     // Disjoint
+        if (lx >= l and rx <= r) return tree[x]; // Fully inside
+        int mid = lx + (rx - lx) / 2;
+        return op(query(l, r, 2 * x + 1, lx, mid), query(l, r, 2 * x + 2, mid, rx));
+    }
+
+public:
+    SegTree(int n, T def = T()) : def(def) {
+        size = 1;
+        while (size < n) size *= 2;
+        tree.assign(2 * size, def);
+    }
+    
+    SegTree(const vector<T>& a, T def = T()) : SegTree(a.size(), def) {
+        build(0, 0, size, a);
+    }
+
+    void update(int i, T v) {
+        update(i, v, 0, 0, size);
+    }
+    void add(int i, T v) {
+        update(i, op(get(i), v));
+    }
+
+    // [l, r)
+    T query(int l, int r) {
+        return query(l, r, 0, 0, size);
+    }
+    T get(int i) {
+        return query(i, i + 1);
+    }
+};
+
+void solve() {
+    int n, q; cin >> n >> q;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+    vector<int> d(n + 1);
+    for (int i = 1; i < n; ++i) d[i] = abs(a[i + 1] - a[i]);
+
+    SegTree<int, decltype([](const int & x, const int & y) -> int { return gcd(x, y); })> st(d, 0ll);
+
+    while (q--) {
+        int l, r; cin >> l >> r;
+        cout << st.query(l, r) << " \n"[q == 0];
+    }
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
