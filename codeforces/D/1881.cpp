501bfe1b74a16e08ae3be79e8aae6236f96a6937
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-13 20:42:31 +0300

    Work Day 210 Commit 01

---
+++ codeforces/D/1881.cpp
@@ -0,0 +1,75 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+const int N = 1e6 + 10;
+vector<int> primes, spf(N+1);
+static const int PrimesAndSPF=[](){
+    for(int i=2;i<=N;i++){ if(!spf[i]) spf[i]=i,primes.push_back(i); for(int p:primes){ if(1LL*p*i>N) break; spf[p*i]=p; if(p==spf[i]) break; } } return 0;
+}();
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n); cin >> a;
+
+    map<int, int> freq;
+    auto count = [&](int x) -> void {
+        while (x != 1) {
+            freq[spf[x]]++;
+            x /= spf[x];
+        }
+    };
+
+    for (int i = 0; i < n; ++i) count(a[i]);
+
+    for (auto &[key, cnt] : freq) {
+        if (cnt % n) return void(cout << "NO\n");
+    }
+    cout << "YES\n";
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
