f2e3bddbe07c13aa88df91100654608e9a456e8d
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-19 17:40:44 +0300

    Work Day 213 Commit 04

---
+++ codeforces/edu/Segment Tree, part 2/Step 2/E.cpp
@@ -0,0 +1,96 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int MOD = 1e9 + 7;
+
+struct Node {
+    int op;
+    int mn;
+};
+
+struct SegTree {
+    int size;
+    vector<Node> tree;
+
+    void init(int n) {
+        size = 1;
+        while (size < n) size <<= 1;
+        tree.assign(2 * size, {-1, 0});
+    }
+
+    void add(int l, int r, int v, int x, int lx, int rx) {
+        if (lx >= r or l >= rx) return;
+        if (lx >= l and rx <= r) {
+            tree[x].op = v;
+            tree[x].mn = v;
+            return;
+        }
+
+        if (~tree[x].op and rx - lx > 1) {
+            tree[2 * x + 1].op = tree[x].op;
+            tree[2 * x + 1].mn = tree[x].op;
+            tree[2 * x + 2].op = tree[x].op;
+            tree[2 * x + 2].mn = tree[x].op;
+            tree[x].op = -1;
+        }
+
+        int mid = (lx + rx) / 2;
+        add(l, r, v, 2 * x + 1, lx, mid);
+        add(l, r, v, 2 * x + 2, mid, rx);
+
+        tree[x].mn = min(tree[2 * x + 1].mn, tree[2 * x + 2].mn);
+    }
+    void add(int l, int r, int v) {
+        add(l, r, v, 0, 0, size);
+    }
+
+    int get(int l, int r, int x, int lx, int rx) {
+        if (lx >= r or l >= rx) return LLONG_MAX;
+        if (lx >= l and rx <= r) {
+            return tree[x].mn;
+        }
+
+        if (~tree[x].op and rx - lx > 1) {
+            tree[2 * x + 1].op = tree[x].op;
+            tree[2 * x + 1].mn = tree[x].op;
+            tree[2 * x + 2].op = tree[x].op;
+            tree[2 * x + 2].mn = tree[x].op;
+            tree[x].op = -1;
+        }
+
+        int mid = (lx + rx) / 2;
+        int m1 = get(l, r, 2 * x + 1, lx, mid);
+        int m2 = get(l, r, 2 * x + 2, mid, rx);
+
+        return min(m1, m2);
+    }
+    int get(int l, int r) {
+        return get(l, r, 0, 0, size);
+    }
+};
+
+signed main() {
+    ios::sync_with_stdio(false);
+
+    int n, q; cin >> n >> q;
+
+    SegTree st;
+    st.init(n);
+
+    while (q--) {
+        int op; cin >> op;
+        if (op == 1) {
+            int l, r, v; cin >> l >> r >> v;
+            st.add(l, r, v);
+        }
+        else {
+            int l, r; cin >> l >> r;
+            cout << st.get(l, r) << endl;
+        }
+    }
+}
