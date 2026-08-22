6141d90a0691242602d8108895c11ece9283e1d4
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-08-22 11:35:33 +0300

    Work Day 215 Commit 04

---
+++ codeforces/gym/The 2025 ICPC Jordanian Collegiate Programming Contest (Mirror)/K.cpp
@@ -0,0 +1,63 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int n; cin >> n;
+    map<char, int> freq;
+    for (int i = 0; i < n; ++i) {
+        char c; cin >> c;
+        freq[c]++;
+    }
+    int ans = 0;
+    for (const auto &[key, cnt] : freq) {
+        ans += cnt / 2;
+    }
+    cout << ans << endl;
+}
+
+const int TESTCASES = 0;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
