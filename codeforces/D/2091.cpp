def9fe692bfb9cb0274be18a92b57d443bf14ff8
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-09-01 18:36:18 +0300

    Work Day 224 Commit 01

---
+++ codeforces/D/2091.cpp
@@ -0,0 +1,43 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int n, m, k; cin >> n >> m >> k;
+    int d = (k + n - 1) / n;
+    int e = m - d;
+    if (e == 0) cout << m << endl;
+    else cout << (d + e) / (e + 1) << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
