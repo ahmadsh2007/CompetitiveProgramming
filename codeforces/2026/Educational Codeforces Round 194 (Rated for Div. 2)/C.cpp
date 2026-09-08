679cea6d878950e940640467979be3d4787db1b5
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2026-09-08 21:22:17 +0300

    Work Day 228 Commit 05

---
+++ codeforces/2026/Educational Codeforces Round 194 (Rated for Div. 2)/C.cpp
@@ -0,0 +1,70 @@
+/*
+       بسم الله الرحمن الرحيم
+    أسالك يا الله التوفيق والنجاح
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Vector input/output operators
+// I wish I could just do `v = list(map(int, input().split()))` instead of this boilerplate
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto& x:v) in>>x;
+    return in;
+}
+// I wish I could just do `print(*v)` instead of this boilerplate
+template<class T>
+ostream& operator<<(ostream& out, vector<T>& v){
+    for(auto& x:v) out<<x<<' ';
+    return out;
+}
+
+#define endl '\n'
+#define int long long
+#define str string // What a Python
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+
+static const int IO_SPEEDUP = [](){
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+    int x, y; cin >> x >> y;
+    int s = x + y;
+    int op = 0;
+    bool flag = false;
+    for (int bit = 29; bit >= 0; --bit) {
+        int bitS = (s >> bit) & 1;
+        int bitX = (x >> bit) & 1;
+
+        if (flag) {
+            if (bitS == 1) op |= (1LL << bit);
+        }
+        else {
+            if (bitS == 1 and bitX == 1) op |= (1LL << bit);
+            else if (bitS == 0 and bitX == 1) flag = true;
+        }
+    }
+
+    cout << s << ' ' << x - op << endl;
+}
+
+const int TESTCASES = 1;
+signed main() {
+    // print("Leeking"); // Yes, it works and yes, it's Python
+
+    int TTT = 1;
+    if (TESTCASES) cin >> TTT;
+    while (TTT--) solve();
+    return 0;
+}
